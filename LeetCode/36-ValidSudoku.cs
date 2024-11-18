5f67be696a868817eba1c41d8ed1e8daf115942a
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-11-18 10:50:46 +0100

    added 36 and 49

---
+++ LeetCode/36-ValidSudoku.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Task36
+{
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            var size = board.Length;
+            var rowMap = new HashSet<string>(size);
+            var columnMap = new HashSet<string>(size);
+            var boxMap = new HashSet<string>(size);
+
+            for (var row = 0; row < board.Length; row++)
+            {
+                for (var column = 0; column < board[row].Length; column++)
+                {
+                    var item = board[row][column];
+                    if (item == '.')
+                        continue;
+
+                    if (!rowMap.Add($"{row}-{item}"))
+                        return false;
+
+                    if (!columnMap.Add($"{column}-{item}"))
+                        return false;
+
+                    int blockColumn = column / 3;
+                    int blockRow = row / 3;
+                    if (!boxMap.Add($"{blockRow * 3 + blockColumn}-{item}"))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/49-GroupAnagrams.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Task49
+{
+    public class GroupAnagramsSolution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var map = new Dictionary<string, List<string>>(strs.Length);
+
+            foreach (var str in strs)
+            {
+                var strCount = new char[26];
+                foreach (var letter in str)
+                {
+                    strCount[letter - 'a']++;
+                }
+
+                var strCode = new string(strCount);
+                if (!map.ContainsKey(strCode))
+                {
+                    map.Add(strCode, new List<string> { str });
+                }
+                else
+                {
+                    map[strCode].Add(str);
+                }
+            }
+
+            var result = new List<IList<string>>();
+            foreach (var key in map.Keys)
+            {
+                result.Add(map[key]);
+            }
+
+            return result;
+        }
+    }
+}
