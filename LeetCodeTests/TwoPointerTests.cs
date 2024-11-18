b4697fda38cbfdb874303474a6dbae65d2264317
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-11-18 10:45:33 +0100

    updated tests for twpointer

--- LeetCodeTests/TwoPointerTests.cs
+++ LeetCodeTests/TwoPointerTests.cs
@@ -1,4 +1,5 @@
-﻿using LeetCode.Basics;
+﻿using System.Collections;
+using LeetCode.Basics;
 
 namespace LeetCodeTests
 {
@@ -11,14 +12,27 @@ namespace LeetCodeTests
             _twoPointers = new TwoPointers();
         }
 
-        public void TwoSumTests()
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 1, 2 })]
+        [InlineData(new int[] { 2, 3, 4 }, 6, new int[] { 1, 3 })]
+        [InlineData(new int[] { -1, 0 }, -1, new int[] { 1, 2 })]
+        public void TwoSumTests(int[] array, int target, int[] expected)
         {
+            var actual = _twoPointers.TwoSum(array, target);
 
+            Assert.Equal(expected, actual);
         }
 
-        public void ThreeSumTests()
+        [Theory]
+        [InlineData(new int[] { -1, 0, 1, 2, -1, -4 }, new object[] { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } })]
+        [InlineData(new int[] { 0, 1, 1 }, new object[0])]
+        [InlineData(new int[] { 0, 0, 0 }, new object[] { new int[] { 0, 0, 0 } })]
+        public void ThreeSumTests(int[] array, object[] expected)
         {
+            var a = new int[][] { new int[] { 1 } };
+            var actual = _twoPointers.ThreeSum(array);
 
+            Assert.Equal(expected, actual);
         }
     }
 }
