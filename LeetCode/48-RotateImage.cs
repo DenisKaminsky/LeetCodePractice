d1973c44690358e065512bc870d120820adcebf9
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-11-23 13:04:10 +0100

    Task 48

---
+++ LeetCode/48-RotateImage.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Task48
+{
+    public class RotateImage
+    {
+        public void Rotate(int[][] matrix)
+        {
+            var dimensions = matrix.Length;
+            if (dimensions <= 1) 
+                return;
+
+            //step 1: swap diagonally
+            for (var i = 0; i < dimensions; i++)
+            {
+                for (var j = i + 1; j < dimensions; j++)
+                {
+                    var tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+            }
+
+            //step 2: swap vertically
+            for (var i = 0; i < dimensions; i++)
+            {
+                for (var j = 0; j < dimensions / 2; j++)
+                {
+                    var tmp = matrix[i][dimensions - j - 1];
+                    matrix[i][dimensions - j - 1] = matrix[i][j];
+                    matrix[i][j] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Task48Tests.cs
@@ -0,0 +1,56 @@
+﻿using LeetCode.Task48;
+
+namespace LeetCodeTests
+{
+    public class Task48Tests
+    {
+        private RotateImage _solution;
+
+        public static IEnumerable<object[]> Data = new object[][]
+        {
+            new object[]
+            {
+                new []
+                {
+                    new[] { 1, 2, 3},
+                    new[] { 4, 5, 6},
+                    new[] { 7, 8, 9}
+                }
+            },
+            new object[]
+            {
+                new []
+                {
+                    new[] { 5, 1, 9, 11 },
+                    new[] { 2, 4, 8, 10 },
+                    new[] { 13, 3, 6, 7 },
+                    new[] { 15, 14, 12, 16 }
+                }
+            }
+        };
+
+        public Task48Tests()
+        {
+            _solution = new RotateImage();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Rotate(int[][] matrix)
+        {
+            var expected = new int[matrix.Length][];
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                expected[i] = new int[matrix.Length];
+                for (var j = 0; j < matrix.Length; j++)
+                {
+                    expected[i][matrix.Length - j - 1] = matrix[j][i];
+                }
+            }
+
+            _solution.Rotate(matrix);
+            
+            Assert.Equal(expected, matrix);
+        }
+    }
+}
