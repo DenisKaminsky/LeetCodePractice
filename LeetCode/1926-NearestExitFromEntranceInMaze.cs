6778cc04cdd6de60806316d171c7f529f31db57d
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-11-24 20:50:30 +0100

    Added task 1926

---
+++ LeetCode/1926-NearestExitFromEntranceInMaze.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Task1926
+{
+    public class NearestExitFromEntranceInMaze
+    {
+        public int NearestExit(char[][] maze, int[] entrance)
+        {
+            var totalRows = maze.Length;
+            var totalColumns = maze[0].Length;
+
+            var viewed = new HashSet<(int, int)>(totalRows * totalColumns);
+
+            //using BFS algorithm
+            var queue = new Queue<(int Row, int Column, int Distance)>();
+            queue.Enqueue((entrance[0], entrance[1], 0));
+
+            while (queue.Count > 0)
+            {
+                var (currentRow, currentColumn, currentDistance) = queue.Dequeue();
+
+                if (currentRow < 0 || currentRow == totalRows)
+                    continue;
+                if (currentColumn < 0 || currentColumn == totalColumns)
+                    continue;
+                if (maze[currentRow][currentColumn] != '.' || viewed.Contains((currentRow, currentColumn)))
+                    continue;
+
+                if (currentDistance > 0 && (currentRow == 0 || currentRow == totalRows - 1 || currentColumn == 0 || currentColumn == totalColumns - 1))
+                    return currentDistance;
+
+                //mark cell as viewed
+                viewed.Add((currentRow, currentColumn));
+
+                //top
+                queue.Enqueue((currentRow - 1, currentColumn, currentDistance + 1));
+                //bottom
+                queue.Enqueue((currentRow + 1, currentColumn, currentDistance + 1));
+                //left
+                queue.Enqueue((currentRow, currentColumn - 1, currentDistance + 1));
+                //right 
+                queue.Enqueue((currentRow, currentColumn + 1, currentDistance + 1));
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeTests/Task1926Tests.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Task1926;
+
+namespace LeetCodeTests
+{
+    public class Task1926Tests
+    {
+        private NearestExitFromEntranceInMaze _solution;
+
+        public static IEnumerable<object[]> Data = new[]
+        {
+            new object[]
+            {
+                new[]
+                {
+                    new[] { '+', '+', '.', '+' },
+                    new[] { '.', '.', '.', '+' },
+                    new[] { '+', '+', '+', '.' }
+                },
+                new[] { 1, 2 },
+                1
+            },
+            new object[]
+            {
+                new[]
+                {
+                    new[] { '+', '+', '+' },
+                    new[] { '.', '.', '.' },
+                    new[] { '+', '+', '+' }
+                },
+                new[] { 1, 0 },
+                2
+            },
+            new object[]
+            {
+                new[]
+                {
+                    new[] { '.', '+' }
+                },
+                new[] { 0, 0 },
+                -1
+            },
+        };
+
+        public Task1926Tests()
+        {
+            _solution = new NearestExitFromEntranceInMaze();
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void NearestExitTests(char[][] maze, int[] entrance, int expected)
+        {
+            var actual = _solution.NearestExit(maze, entrance);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
